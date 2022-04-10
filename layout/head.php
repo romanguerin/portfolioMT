3e053eb8ce27ecb8b2e8aeccc89926cc8ff90e09
Roman Guerin <romanguerin@msn.com>
2022-04-10 08:11:23 +0200

    head Commit

---
+++ layout/head.php
@@ -0,0 +1,11 @@
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <title>Portfolio</title>
+    <link rel="icon" href="" type="png" sizes="16x16">
+    <link href="http://fonts.googleapis.com/css?family=Abel" rel="stylesheet" type="text/css" />
+    <link href="../style/style.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="../style/responsive.css" rel="stylesheet" type="text/css" media="screen" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="../main.js"></script>
+</head>
