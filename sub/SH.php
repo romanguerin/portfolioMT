dd49f6a867308c74970c7f3c6808a0633d4c4155
Roman Guerin <romanguerin@msn.com>
2022-04-20 17:49:05 +0200

    19 April 2

--- sub/SH.php
+++ sub/SH.php
@@ -3,19 +3,65 @@
         <div id="page-bgbtm">
             <div id="content">
                 <div class="post" id="home">
-                    <h2 class="title">SH</h2>
+                    <h2 class="title">Science & Humanities</h2>
                     <!--The vivid dream reseach post 1-->
                     <div class="entry">
                         <br/>
-                        <p><i>My name is Roman Guerin bla bla</i>
+                        <p><i>Semester 2 2021</i>
                         </p>
                         <br/>
-                        <p>I did a bachelor communication & multi Media Design and my internship at AC3 Studio</strong>
+                        <p>A course about Science & Humanities</p>
+                        <br/>
+                        <h3>Video</h3>
+                        <iframe style="margin: 5px 0 5px 0" width="100%" height="360px"
+                                src="https://youtube.com/embed/zdnm3si1hPM">
+                        </iframe>
+                        <p><i>Science & Humanities Project</i></p>
+                        <br/>
+                        <h3>Paper</h3>
+                        <p><br/>
+                            <strong>Abstract</strong><br/>
+                            While studying for science & humanities, I watched these videos from Leiden University by Victor Gijsbers.
+                            When watching; Chapter 1.4 Karl Popper and the logic of falsification, I noticed a lot of dislikes under the video.
+                            This is interesting because it is a video from the same university where I am studying, and other videos
+                            from him did not seem to have this many dislikes. However, people in the comments thought that the presenter
+                            was failing to comprehend a simple point of Karl Popper. In my own video, I will elaborate on what the presenter
+                            tells us in this video and then compare it with the comments and Poppers original work.
                         </p>
                         <br/>
-                        <p>Well let’s take out the two most important words to better understand the meaning of this question.
-                            <strong>Virtual </strong> and <strong>Experience.</strong>
-                        </p>
+                        <a style ="color: black" href="./pdf/GuerinRoman_S&H_SmallEndProject.pdf" target="_blank">
+                            RomanGuerin Small end Project.pdf<img alt="pdf" id="ico" src="./images/PDF-icon-small.png">
+                        </a>
+                        <p><i>A reflection on Victor Gijsbers view of Karl Popper and the logic of falsification</i><br/>
+                            2021, <strong>Guerin, R.N.</strong></p>
+                        <br/>
+                        <h3>Assignment</h3>
+                        <br/>
+                        <p>Homework Assignment 1<br/>
+                            <a style ="color: black" href="./pdf/GuerinRoman_HomeworkAssigment_1.pdf" target="_blank">
+                                Assignment 1.pdf<img alt="pdf" id="ico" src="./images/PDF-icon-small.png">
+                            </a></p>
+                        <p>Homework Assignment 2<br/>
+                        <a style ="color: black" href="./pdf/GuerinRoman_HomeworkAssigment_2.pdf" target="_blank">
+                            Assignment 2.pdf<img alt="pdf" id="ico" src="./images/PDF-icon-small.png">
+                        </a></p>
+                        <p>Kepler's contribution to the Copernican revolution<br/>
+                            <a style ="color: black" href="./pdf/GuerinRoman_HomeworkAssigment_3.pdf" target="_blank">
+                                Assignment 3.pdf<img alt="pdf" id="ico" src="./images/PDF-icon-small.png">
+                            </a></p>
+                        <p>The Earth is blue<br/>
+                            <a style ="color: black" href="./pdf/GuerinRoman_HomeworkAssignment_4.pdf" target="_blank">
+                                Assignment 4.pdf<img alt="pdf" id="ico" src="./images/PDF-icon-small.png">
+                            </a></p>
+                        <p>There is a dark side of the moon<br/>
+                        <a style ="color: black" href="./pdf/GuerinRoman_HomeworkAssignment_5.pdf" target="_blank">
+                            Assignment 5.pdf<img alt="pdf" id="ico" src="./images/PDF-icon-small.png">
+                        </a></p>
+                        <p>A reflection on 5G-Gekkies (5G-Sillies)<br/>
+                            <a style ="color: black" href="./pdf/GuerinRoman_HomeworkAssignment_7.pdf" target="_blank">
+                                Assignment 6.pdf<img alt="pdf" id="ico" src="./images/PDF-icon-small.png">
+                            </a></p>
+                        <p>2021, <strong>Guerin, R.N.</strong></p>
 
                     </div>
                 </div>
--- sub/SSI.php
+++ sub/SSI.php
@@ -3,19 +3,41 @@
         <div id="page-bgbtm">
             <div id="content">
                 <div class="post" id="home">
-                    <h2 class="title">SSI</h2>
-                    <!--The vivid dream reseach post 1-->
+                    <h2 class="title">Sound Space & Interaction</h2>
                     <div class="entry">
                         <br/>
-                        <p><i>My name is Roman Guerin bla bla</i>
+                        <p><i>Semester 2 2021</i>
                         </p>
                         <br/>
-                        <p>I did a bachelor communication & multi Media Design and my internship at AC3 Studio</strong>
+                        <p>Sound space & Interaction</p>
+                        <br/>
+                        <h3>Video</h3>
+                        <iframe style="margin: 5px 0 5px 0" width="100%" height="360px"
+                                src="https://youtube.com/embed/vjIJd_R3oZ8">
+                        </iframe>
+                        <p><i>Guess!</i></p>
+                        <br/>
+                        <h3>Project Report</h3>
+                        <p>
+                            Imagine that when you’re walking on the street, you hear someone calling your name;
+                            or when you’re alone at night, you hear a strange noise...can you detect which direction
+                            the sounds are coming from? This ability is important for human beings, but there exist
+                            individual differences. Some people are good at telling the source direction of sounds,
+                            while others find it difficult. Therefore, we aimed to build a game in which people are
+                            allowed to try if they are capable of locating the source of sounds.
                         </p>
                         <br/>
-                        <p>Well let’s take out the two most important words to better understand the meaning of this question.
-                            <strong>Virtual </strong> and <strong>Experience.</strong>
-                        </p>
+                        <h3>Report</h3>
+                        <a style ="color: black" href="./pdf/Project report- Guess.pdf" target="_blank">
+                            Project report.pdf<img alt="pdf" id="ico" src="./images/PDF-icon-small.png">
+                        </a>
+                        <p><i>Guess! Where is the sound coming from?</i><br/>
+                            2021, <strong>Guerin, R.N. - Li, J.</strong></p>
+                        <br/>
+                        <h3>Pure Data</h3>
+                        <a style ="color: black" href="./zip/SSI_Guess!_pd.zip" target="_blank">
+                            The PureData and python code from the project<img alt="pdf" id="ico" src="./images/rar.png">
+                        </a>
 
                     </div>
                 </div>
